93bbf648abc5301c696e1d480a1635f8d659b190
goodpeanuts <goodpeanuts@foxmail.com>
2024-12-10 16:42:14 +0800

    builder-02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,30 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    eprintln!("{:#?}", ast);
-    TokenStream::new()
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+    let expanded = quote! {
+       pub struct #bident {
+        executable: Option<String>,
+        args: Option<Vec<String>>,
+        env: Option<Vec<String>>,
+        current_dir: Option<String>,
+       }
+       impl #name {
+           pub fn builder() -> #bident {
+               #bident {
+                executable: None,
+                args: None,
+                env: None,
+                current_dir: None,
+               }
+           }
+       }
+    };
+    expanded.into()
 }
