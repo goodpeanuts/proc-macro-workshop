bc446a368c328753b2d3ad249fafe0e2a70472a6
zenghao.333 <zenghao.333@bytedance.com>
2024-12-12 21:30:01 +0800

    debug-01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", ast);
+    match do_expand(&ast) {
+        Ok(expanded) => expanded.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
 
-    unimplemented!()
+fn do_expand(_ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let rt = proc_macro2::TokenStream::new();
+    return Ok(rt);
 }
