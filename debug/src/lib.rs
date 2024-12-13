82c41d8f0e8a081daad0c29a7ebcd8095681da05
goodpeanuts <goodpeanuts@foxmail.com>
2024-12-13 14:28:22 +0800

    debug-03-custom-format

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,10 +2,10 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    // eprintln!("{:#?}", ast);
+    // println!("{:#?}", ast);
     match do_expand(&ast) {
         Ok(expanded) => expanded.into(),
         Err(e) => e.to_compile_error().into(),
@@ -15,15 +15,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn do_expand(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let struct_name = &ast.ident;
     let fields = get_fields(ast)?;
-    let idents = fields.iter().map(|f| &f.ident).collect::<Vec<_>>();
+    let format_string_def = generate_debug_format_string(fields)?;
 
     let expanded = quote! {
         impl std::fmt::Debug for #struct_name {
             fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
                 fmt.debug_struct(std::stringify!(#struct_name))
-                    // .field("name", &self.name)
-                    // .field("age", &self.age)
-                    #( .field(std::stringify!(#idents), &self.#idents))*
+                    #format_string_def
                     .finish()
             }
         }
@@ -47,4 +45,42 @@ fn get_fields(ast: &syn::DeriveInput) -> syn::Result<&StructFields> {
             "only structs with named fields are supported",
         ))
     }
+}
+
+fn get_debug_format_string(field: &syn::Field) -> syn::Result<std::option::Option<String>> {
+    for attr in &field.attrs {
+        if attr.path().is_ident("debug") {
+            if let syn::Meta::NameValue(ref meta_name_value) = attr.meta {
+                if let syn::Expr::Lit(ref expr_lit) = meta_name_value.value {
+                    if let syn::Lit::Str(ref lit_str) = expr_lit.lit {
+                        return Ok(Some(lit_str.value()));
+                    }
+                }
+            }
+        } else {
+            return Err(syn::Error::new_spanned(
+                attr.path(),
+                "only #[debug] is supported",
+            ));
+        }
+    }
+    Ok(None)
+}
+
+fn generate_debug_format_string(fields: &StructFields) -> syn::Result<proc_macro2::TokenStream> {
+    let mut format_string = proc_macro2::TokenStream::new();
+    for field in fields {
+        let ident = &field.ident;
+
+        if let Some(debug_format_string) = get_debug_format_string(field)? {
+            format_string.extend(quote::quote! {
+                .field(std::stringify!(#ident), &format_args!(#debug_format_string, self.#ident))
+            });
+        } else {
+            format_string.extend(quote::quote! {
+                .field(std::stringify!(#ident), &self.#ident)
+            });
+        }
+    }
+    Ok(format_string)
 }
