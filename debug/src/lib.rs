ff22bb711ff0ce815968e6604d7014a98200c6bf
zenghao.333 <zenghao.333@bytedance.com>
2024-12-16 17:17:57 +0800

    debug-05-phantom-data & 06-bound-trouble

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -17,14 +17,28 @@ fn do_expand(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let fields = get_fields(ast)?;
     let format_string_def = generate_debug_format_string(fields)?;
 
+    let mut struct_member_types = Vec::new();
+    let mut phantomdata_inner_types = Vec::new();
+    for field in fields {
+        if let Some(s) = get_struct_member_type(field)? {
+            struct_member_types.push(s);
+        }
+        if let Some(s) = get_phantomdata_innner_generic(field)? {
+            phantomdata_inner_types.push(s);
+        }
+    }
+
     let mut generics = ast.generics.clone();
     generics.params.iter_mut().for_each(|generic| {
         if let syn::GenericParam::Type(ref mut type_param) = generic {
-            type_param.bounds.push(syn::parse_quote!(std::fmt::Debug));
+            let ty_str = type_param.ident.to_string();
+            if phantomdata_inner_types.contains(&ty_str) && !struct_member_types.contains(&ty_str) {
+            } else {
+                type_param.bounds.push(syn::parse_quote!(std::fmt::Debug));
+            }
         }
     });
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
 
     let expanded = quote! {
         impl #impl_generics std::fmt::Debug for #struct_name #ty_generics #where_clause {
@@ -59,12 +73,16 @@ fn get_fields(ast: &syn::DeriveInput) -> syn::Result<&StructFields> {
 fn get_debug_format_string(field: &syn::Field) -> syn::Result<std::option::Option<String>> {
     for attr in &field.attrs {
         if attr.path().is_ident("debug") {
-            if let syn::Meta::NameValue(ref meta_name_value) = attr.meta {
-                if let syn::Expr::Lit(ref expr_lit) = meta_name_value.value {
-                    if let syn::Lit::Str(ref lit_str) = expr_lit.lit {
-                        return Ok(Some(lit_str.value()));
-                    }
-                }
+            if let syn::Meta::NameValue(syn::MetaNameValue {
+                value:
+                    syn::Expr::Lit(syn::ExprLit {
+                        lit: syn::Lit::Str(ref lit_str),
+                        ..
+                    }),
+                ..
+            }) = attr.meta
+            {
+                return Ok(Some(lit_str.value()));
             }
         } else {
             return Err(syn::Error::new_spanned(
@@ -74,6 +92,49 @@ fn get_debug_format_string(field: &syn::Field) -> syn::Result<std::option::Optio
         }
     }
     Ok(None)
+}
+
+fn get_phantomdata_innner_generic(field: &syn::Field) -> syn::Result<Option<String>> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { ref segments, .. },
+        ..
+    }) = field.ty
+    {
+        if let Some(syn::PathSegment {
+            ref ident,
+            ref arguments,
+        }) = segments.last()
+        {
+            if ident == "PhantomData" {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    args,
+                    ..
+                }) = arguments
+                {
+                    if let Some(syn::GenericArgument::Type(syn::Type::Path(ref gp))) = args.first()
+                    {
+                        if let Some(generic_ident) = gp.path.segments.first() {
+                            return Ok(Some(generic_ident.ident.to_string()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return Ok(None);
+}
+
+fn get_struct_member_type(field: &syn::Field) -> syn::Result<Option<String>> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { ref segments, .. },
+        ..
+    }) = field.ty
+    {
+        if let Some(syn::PathSegment { ref ident, .. }) = segments.last() {
+            return Ok(Some(ident.to_string()));
+        }
+    }
+    return Ok(None);
 }
 
 fn generate_debug_format_string(fields: &StructFields) -> syn::Result<proc_macro2::TokenStream> {
