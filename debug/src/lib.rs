a9f3f8ad8cc59ca7faad4966fb032a8fc204ebe3
goodpeanuts <goodpeanuts@foxmail.com>
2024-12-16 19:48:13 +0800

    debug-07-associated-type

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,5 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use std::collections::HashMap;
+use syn::visit::{self, Visit};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -19,6 +21,8 @@ fn do_expand(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
 
     let mut struct_member_types = Vec::new();
     let mut phantomdata_inner_types = Vec::new();
+    let associated_types = get_generic_associated_types(ast);
+
     for field in fields {
         if let Some(s) = get_struct_member_type(field)? {
             struct_member_types.push(s);
@@ -29,17 +33,29 @@ fn do_expand(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     }
 
     let mut generics = ast.generics.clone();
+
     generics.params.iter_mut().for_each(|generic| {
         if let syn::GenericParam::Type(ref mut type_param) = generic {
             let ty_str = type_param.ident.to_string();
             if phantomdata_inner_types.contains(&ty_str) && !struct_member_types.contains(&ty_str) {
+            } else if associated_types.contains_key(&ty_str)
+                && !struct_member_types.contains(&ty_str)
+            {
             } else {
                 type_param.bounds.push(syn::parse_quote!(std::fmt::Debug));
             }
         }
     });
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
+    generics.make_where_clause();
+    for (_, associated_types) in &associated_types {
+        for associated_type in associated_types {
+            generics.where_clause.as_mut().unwrap().predicates.push(syn::parse_quote!(#associated_type:std::fmt::Debug));
+        }
+    }
+    
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    
     let expanded = quote! {
         impl #impl_generics std::fmt::Debug for #struct_name #ty_generics #where_clause {
             fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -153,4 +169,52 @@ fn generate_debug_format_string(fields: &StructFields) -> syn::Result<proc_macro
         }
     }
     Ok(format_string)
+}
+
+struct TypePathVisitor {
+    generic_type_names: Vec<String>,
+    associated_types: HashMap<String, Vec<syn::TypePath>>,
+}
+
+impl<'ast> Visit<'ast> for TypePathVisitor {
+    // visit_type_path 这个回调函数就是我们所关心的
+    fn visit_type_path(&mut self, node: &'ast syn::TypePath) {
+        if node.path.segments.len() >= 2 {
+            let generic_type_name = node.path.segments[0].ident.to_string();
+            if self.generic_type_names.contains(&generic_type_name) {
+                // 如果满足上面的两个筛选条件，那么就把结果存起来
+                self.associated_types
+                    .entry(generic_type_name)
+                    .or_insert(Vec::new())
+                    .push(node.clone());
+            }
+        }
+        // Visit 模式要求在当前节点访问完成后，继续调用默认实现的visit方法，从而遍历到所有的
+        // 必须调用这个函数，否则遍历到这个节点就不再往更深层走了
+        visit::visit_type_path(self, node);
+    }
+}
+
+fn get_generic_associated_types(st: &syn::DeriveInput) -> HashMap<String, Vec<syn::TypePath>> {
+    // 首先构建筛选条件
+    let origin_generic_param_names: Vec<String> = st
+        .generics
+        .params
+        .iter()
+        .filter_map(|f| {
+            if let syn::GenericParam::Type(ty) = f {
+                return Some(ty.ident.to_string());
+            }
+            return None;
+        })
+        .collect();
+
+    let mut visitor = TypePathVisitor {
+        generic_type_names: origin_generic_param_names, // 用筛选条件初始化Visitor
+        associated_types: HashMap::new(),
+    };
+
+    // 以st语法树节点为起点，开始Visit整个st节点的子节点
+    visitor.visit_derive_input(st);
+    return visitor.associated_types;
 }
